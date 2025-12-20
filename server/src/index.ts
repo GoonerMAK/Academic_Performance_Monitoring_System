21ac07fe89537f629dd45c10b5c3a0caf8634f2e
Mashrur Ahsan (Akash) <mashrurahsan@iut-dhaka.edu>
2025-12-20 20:52:55 +0600

    implemented auth routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import cookieParser from 'cookie-parser';
 import dotenv from "dotenv";
+
+import { authRouter } from "./routes/auth.routes.js";
 
 export const app = express()
 dotenv.config();
 
 app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/auth', authRouter);
 
 const PORT = process.env.PORT;
 export const server = app.listen(PORT, () => {
