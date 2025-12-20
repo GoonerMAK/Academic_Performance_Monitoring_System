0fab4b81d2bbf39fa79188c4199166293f0b5aae
Mashrur Ahsan (Akash) <mashrurahsan@iut-dhaka.edu>
2025-12-20 20:55:08 +0600

    implemented validator, service, controller, routes for user module

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cookieParser from 'cookie-parser';
 import dotenv from "dotenv";
 
+import { userRouter } from "./routes/user.route.js";
 import { authRouter } from "./routes/auth.routes.js";
 
 export const app = express()
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth', authRouter);
+app.use('/api', userRouter);
 
 const PORT = process.env.PORT;
 export const server = app.listen(PORT, () => {
